2dc29cfc8db051c75af0356761446cb30ad58e9a
Patryk Kotarski <patrykkotarski21@gmail.com>
2025-01-13 03:08:49 +0100

    Update exports

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -3,12 +3,8 @@ export HOMEBREW_NO_ANALYTICS=1
 
 # NVM
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# NVIM
-# export NVIM_APPNAME="NvChad"
-# export EDITOR=nvim
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/vigenerr/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/vigenerr/google-cloud-sdk/path.zsh.inc'; fi
@@ -20,9 +16,6 @@ if [ -f '/Users/vigenerr/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/
 
 # PKCFG
 export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgconfig"
-
-# Toptal infra tools
-export PATH=/opt/homebrew/bin:$PATH
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
