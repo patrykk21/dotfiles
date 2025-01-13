e02908065dfeb13698d4404e275a3b8cccbc14ee
Patryk Kotarski <patrykkotarski21@gmail.com>
2025-01-13 13:44:43 +0100

    Use updated fzf

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -6,8 +6,6 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 # PKCFG
 export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgconfig"
 
