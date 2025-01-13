a0d00cbda4dca0baab3dbb9980bde091060521b6
Patryk Kotarski <vigenerr@a5f1f26b-527e-4be0-9ddb-72da17077538.fritz.box>
2025-01-13 13:42:29 +0100

    Update config

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -6,12 +6,6 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/vigenerr/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/vigenerr/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/Users/vigenerr/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/vigenerr/google-cloud-sdk/completion.zsh.inc'; fi
-
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # PKCFG
@@ -19,6 +13,13 @@ export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgco
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
-source ~/.rvm/scripts/rvm
-rvm use default
 
+# Volta
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/vigenerr/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/vigenerr/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/vigenerr/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/vigenerr/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
